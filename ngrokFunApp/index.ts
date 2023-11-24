658873867505e76f7f8e53e0aaf20f48a3bc0eb7
Shubham Kumar <shuku@microsoft.com>
2023-11-24 11:57:47 +0530

    api call added

--- ngrokFunApp/index.ts
+++ ngrokFunApp/index.ts
@@ -1,8 +1,12 @@
 import { AzureFunction, Context, HttpRequest, HttpResponse } from "@azure/functions"
 import fetch from 'node-fetch';
+import { readFileSync } from 'fs';
+import tunnels404 from './tunnels404.html'
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');    
+    // const tunnels404 = '<center><h2> No Tunnels Found !!! </h2></center>';
+    // const tunnels404 = readFileSync('./tunnels404.html');
 
     const link = `<a href=__URL__> Partner 360 POC </a>`;
     const respp:HttpResponse = {
@@ -34,7 +38,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
             });
         }
         if(body === ''){
-            body = '<center><h2> No Tunnels Found !!! </h2></center>'
+            body = tunnels404.toLocaleString() ;
         }
         respp.body = body;
         context.res = respp;
