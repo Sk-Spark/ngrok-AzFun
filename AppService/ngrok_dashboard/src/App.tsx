8e7440a94b4dc5b8e28985f55d62e2b14199effa
Shubham Kumar <shuku@microsoft.com>
2023-11-25 11:44:27 +0530

    UI update

--- AppService/ngrok_dashboard/src/App.tsx
+++ AppService/ngrok_dashboard/src/App.tsx
@@ -1,48 +1,61 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { makeNgrokApiRequest } from './actions';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import loadingSvg from "./loading.svg";
+import "./App.css";
+import { makeNgrokApiRequest } from "./actions";
 
 export const App = () => {
-
-  const [tunnel, setTunnel] = React.useState({public_url:[]});
+  const [tunnel, setTunnel] = React.useState({ public_url: [] });
+  const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
     makeNgrokApiRequest().then((data) => {
-      console.log("#1",data);
-      if(data.tunnels.length > 0) {
-        console.log("#1 tunnel url:",data.tunnels[0].public_url);
-        const tunnels:any = [];
-        data.tunnels.forEach((tunnel:any) => {
+      console.log("#1", data);
+      if (data.tunnels.length > 0) {
+        console.log("#1 tunnel url:", data.tunnels[0].public_url);
+        const tunnels: any = [];
+        data.tunnels.forEach((tunnel: any) => {
           tunnels.push(tunnel.public_url);
         });
-        setTunnel({public_url: tunnels});
+        setTunnel({ public_url: tunnels });
       }
-
+      setLoading(false);
     });
   }, []);
 
   const getTunnelLinks = () => {
     return tunnel.public_url.map((url) => {
-      return <div key={url}> 
-        <a 
-          className="App-link" 
-          href={url}
-          target='_blank'
-        >
+      return (
+        <div key={url}>
+          <a className="App-link" href={url} target="_blank">
             {url}
-        </a> 
-      </div>
+          </a>
+        </div>
+      );
     });
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ngrok Dashboard</h1>
-        {getTunnelLinks()}
+        {!loading && tunnel.public_url.length > 0 && getTunnelLinks()}
+        {!loading && tunnel.public_url.length == 0 && (
+          <div>
+            <p>
+              Ngrok is not running. <br/>
+              Please start ngrok and refresh this page.
+            </p>
+          </div>
+        )}
+
+        {loading && 
+          <>
+            <img src={loadingSvg} width={100} className="App-logo" alt="loading" />
+            <label>Loading Tunnels ...</label>
+          </>
+        }
       </header>
     </div>
   );
-}
-
+};
